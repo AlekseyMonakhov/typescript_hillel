b555eeb728faa633a1525760beffbe858780cf09
Aleksey Monakhov <goshagriboedskii@gmail.com>
2023-10-03 22:45:21 +0300

    upd

--- HW8/src/index.ts
+++ HW8/src/index.ts
@@ -1,47 +1,8 @@
-function filterArr<T>(arr: T[], condition: (item: T) => boolean): T[] {
-  return arr.filter(condition);
+function cb(name: never, age: number): void {
+  console.log(age);
 }
 
-class Stack<T> {
-  private items: T[] = [];
 
-  private isEmpty(): boolean {
-    return !!this.items.length;
-  }
-
-  public push(item: T): void {
-    this.items.push(item);
-  }
-
-  public pop(): T | undefined {
-    if (this.isEmpty()) {
-      throw new Error('Stack is empty');
-    }
-
-    return this.items.pop();
-  }
-
-  public peek(): T | undefined {
-    if (this.isEmpty()) {
-      throw new Error('Stack is empty');
-    }
-
-    return this.items[this.items.length - 1];
-  }
-}
-
-class Dictionary<K extends string | number | symbol, V> {
-  private items: Record<K, V> = {} as Record<K, V>;
-
-  public set(key: K, value: V): void {
-    this.items[key] = value;
-  }
-
-  public get(key: K): V {
-    return this.items[key];
-  }
-
-  public has(key: K): boolean {
-    return key in this.items;
-  }
+function test(cd:(name:never, age:number): void):void {
+  cd("Alex", 24);
 }
---
+++ HW9/src/index.ts
@@ -0,0 +1,17 @@
+
+type DeepReadonly<T extends object> = {
+  readonly [K in keyof T]: T[K] extends object ? DeepReadonly<T[K]> : T[K];
+};
+
+type DeepRequireReadonly<T> = {
+  readonly [K in keyof T]-?: T[K] extends object ? DeepRequireReadonly<T[K]> : T[K];
+};
+
+type UpperCaseKeys<T extends object> = {
+  [K in keyof T as Uppercase<string & K>]: T[K];
+};
+
+type ObjectToPropertyDescriptor<T> = {
+  [K in keyof T]: PropertyDescriptor;
+};
+
